c263badfdcd062bc263dc94d76d46e5fa7364d64
Drew Early <61104507+pteranodog@users.noreply.github.com>
2025-02-15 18:58:49 -0600

    Convert to PlatformIO and use header files

---
+++ src/board-io/sensor-implementations/M9N.cpp
@@ -0,0 +1,19 @@
+#include <data.h>
+#include <board-io/sensors.h>
+
+void M9N::init() {
+    // TODO
+}
+
+Position M9N::getPosition() {
+    // TODO
+    // Make sure to convert altitude from mm to m
+}
+
+UTCTime M9N::getUTCTime() {
+    // TODO
+}
+
+int M9N::getSIV() {
+    // TODO
+}
