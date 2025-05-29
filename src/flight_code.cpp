534305e2b7c63e8629ead39b85381027dffd1152
Jude7102 <jp0182@uah.edu>
2025-05-29 16:17:11 -0500

    NG Pins set added

--- src/flight_code.cpp
+++ src/flight_code.cpp
@@ -27,6 +27,24 @@ void setup() {
   setSolenoids(SOLENOIDS_OFF);
   
   happyHavocLightDance();
+
+  errorLED.temporaryColor(colorPresets.blue);
+  delay(10000);
+  errorLED.clearTemporaryColor();
+
+    //NG Pins Set
+  for (int i = 0; i < 4; i++)
+  { 
+    digitalWrite(config.pins.SDN, HIGH);
+    digitalWrite(config.pins.NGPOWER, HIGH);
+    delay(5000);
+    digitalWrite(config.pins.SDN, LOW);
+    digitalWrite(config.pins.NGPOWER, HIGH);
+    delay(5000);
+    digitalWrite(config.pins.SDN, LOW);
+    digitalWrite(config.pins.NGPOWER, LOW);
+    delay(5000);
+  }
 }
 
 void loop() {
