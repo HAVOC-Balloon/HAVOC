a63ec05cceb1610851024d88a6665bb8c4839a7b
Drew Early <jae0025@uah.edu>
2025-11-12 00:53:44 -0600

    Removed NG code from non-NG flight_code.cpp

--- src/flight_code.cpp
+++ src/flight_code.cpp
@@ -4,20 +4,10 @@
 
 void setup() {
   initPins();
-  setSolenoids(SOLENOIDS_OFF);
-  delay(500);
-  setSolenoids(COUNTERCLOCKWISE);
-  delay(100);
-  setSolenoids(SOLENOIDS_OFF);
-  delay(500);
-  setSolenoids(CLOCKWISE);
-  delay(100);
-  setSolenoids(SOLENOIDS_OFF);
   logger.init();
   sensors.barometer.init();
   sensors.imu.init();
   sensors.gps.init();
-
   setSolenoids(SOLENOIDS_OFF);
   delay(500);
   setSolenoids(CLOCKWISE);
@@ -25,60 +15,20 @@ void setup() {
   setSolenoids(COUNTERCLOCKWISE);
   delay(50);
   setSolenoids(SOLENOIDS_OFF);
-  
   happyHavocLightDance();
-
-  errorLED.temporaryColor(colorPresets.blue);
-  delay(5000);
-  errorLED.clearTemporaryColor();
-
-    //NG Pins Set
-  for (int i = 0; i < 2; i++)
-  { 
-    digitalWrite(config.pins.SDN, LOW); //Actual HIGH
-    digitalWrite(config.pins.NGPOWER, LOW); //Actual HIGH
-    delay(10000);
-
-    errorLED.temporaryColor(colorPresets.red);
-    delay(500);
-    errorLED.clearTemporaryColor();
-
-    digitalWrite(config.pins.SDN, LOW); //Actual HIGH
-    digitalWrite(config.pins.NGPOWER, HIGH); //Actual LOW
-    delay(10000);
-
-    errorLED.temporaryColor(colorPresets.red);
-    delay(500);
-    errorLED.clearTemporaryColor();
-
-    digitalWrite(config.pins.SDN, HIGH); // Actual LOW
-    digitalWrite(config.pins.NGPOWER, HIGH); //Actual LOW
-    delay(10000);
-    
-    errorLED.temporaryColor(colorPresets.red);
-    delay(500);
-    errorLED.clearTemporaryColor();
-  }
-
-  errorLED.temporaryColor(colorPresets.red);
-  delay(2000);
-  errorLED.clearTemporaryColor();
 }
 
 void loop() {
   static Timer loopTimer(config.loopCycleTime);
   data.packetCount += 1;
   data.missionTime = millis();
-  //blinkLEDs();
-  //digitalWrite(config.pins.SDN, HIGH);
-  //digitalWrite(config.pins.NGPOWER, HIGH); 
+  blinkLEDs();
   sensors.imu.collectData(data);
   sensors.gps.collectData(data);
   sensors.barometer.collectData(data);
   updateFlightState();
   stateActions();
   logger.writeTelemetry(data);
-  while (!loopTimer.isComplete()) {
-  }
+  while (!loopTimer.isComplete()) {}
   loopTimer.reset();
 }
