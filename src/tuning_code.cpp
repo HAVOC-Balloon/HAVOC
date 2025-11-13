b271356b3bd47b2e73d572301e32930a520947d6
Sa9m <abyssalflerken@gmail.com>
2025-11-12 19:55:38 -0600

    Added tuning env

---
+++ src/tuning_code.cpp
@@ -0,0 +1,34 @@
+#include <Arduino.h>
+
+#include "havoc.h"
+
+void setup() {
+  initPins();
+  logger.init();
+  sensors.barometer.init();
+  sensors.imu.init();
+  sensors.gps.init();
+  setSolenoids(SOLENOIDS_OFF);
+  delay(500);
+  setSolenoids(CLOCKWISE);
+  delay(50);
+  setSolenoids(COUNTERCLOCKWISE);
+  delay(50);
+  setSolenoids(SOLENOIDS_OFF);
+  happyHavocLightDance();
+}
+
+void loop() {
+  static Timer loopTimer(config.loopCycleTime);
+  data.packetCount += 1;
+  data.missionTime = millis();
+  blinkLEDs();
+  sensors.imu.collectData(data);
+  sensors.gps.collectData(data);
+  sensors.barometer.collectData(data);
+  data.state = FlightState::STABILIZATION;
+  stateActions();
+  logger.writeTelemetry(data);
+  while (!loopTimer.isComplete()) {}
+  loopTimer.reset();
+}
